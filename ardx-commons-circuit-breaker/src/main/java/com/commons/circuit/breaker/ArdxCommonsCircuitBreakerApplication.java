acf096b6e687c24351bf3a4165092436553e3d42
Oliveira <santiago.oliveira@accenture.com>
2022-06-16 03:00:49 -0300

    test

---
+++ ardx-commons-circuit-breaker/src/main/java/com/commons/circuit/breaker/ArdxCommonsCircuitBreakerApplication.java
@@ -0,0 +1,8 @@
+package com.commons.circuit.breaker;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdxCommonsCircuitBreakerApplication {
+
+}
---
+++ ardx-commons-circuit-breaker/src/test/java/com/commons/circuit/breaker/ArdxCommonsCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commons.circuit.breaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArdxCommonsCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
